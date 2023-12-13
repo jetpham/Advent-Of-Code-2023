859be9f003742f162f6376f3cf2835c634208402
Jet Pham <55770902+jetpham@users.noreply.github.com>
2023-12-13 00:48:19 -0800

    Finished Day 1!

---
+++ Day1.py
@@ -0,0 +1,45 @@
+from typing import List
+
+with open("Day1Input.txt", "r") as file:
+    input = file.readlines()
+
+def trebuchet(noisyValues: List[str]) -> int:
+    total = 0
+    valid = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+        "1": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9
+    }
+    for string in noisyValues:
+        print(f"Processing string: {string}")
+        values = []
+        i = 0
+        while i < len(string):
+            for key in valid:
+                if string[i:].startswith(key):
+                    values.append(valid[key])
+                    i += len(key)
+                    break
+            else:
+                i += 1
+        print(f"Values extracted from string: {values}")
+        total += values[0] * 10 + values[-1]
+    print(f"Total sum: {total}")
+    return total
+
+print(trebuchet(input))
