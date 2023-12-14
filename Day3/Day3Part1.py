d65636ff458fd39d3095cfc738dce2fc92b7c48d
Jet Pham <55770902+jetpham@users.noreply.github.com>
2023-12-13 18:53:04 -0800

    newlines were in the input wtf

--- Day3/Day3Part1.py
+++ Day3/Day3Part1.py
@@ -5,7 +5,9 @@ current_dir = os.path.dirname(__file__)
 file_path = os.path.join(current_dir, "input.txt")
 
 with open(file_path, "r") as file:
-    input = file.readlines()
+    inputFile = [line.rstrip() for line in file.readlines()]
+
+
 # The engine schematic (your puzzle input) consists of a visual representation of the engine. There are lots of numbers and symbols you don't really understand, but apparently any number adjacent to a symbol, even diagonally, is a "part number" and should be included in your sum. (Periods (.) do not count as a symbol.)
 
 # Here is an example engine schematic:
@@ -32,70 +34,40 @@ with open(file_path, "r") as file:
 # if any of those are symbols then add the number to the total and skip to the end of the number/ keep going.
 
 
-def Day3(input: List[str]) -> int:
-    total = 0
-    for rowIndex, row in enumerate(input):
-        length = 0
-        numberStartingIndex = None
-        for columnIndex, char in enumerate(row):
-            if numberStartingIndex is None:
-                if char.isdigit():
-                    numberStartingIndex = columnIndex
-                    length += 1
-                else:
-                    continue
-            if char.isdigit():
-                length += 1
-            else:
-                if length == 0 and numberStartingIndex is None:
-                    continue
-                else:
-                    borderingSymbols = []
-                    if columnIndex != 0:
-                        #getting the symbol to the left of the number
-                        borderingSymbols.append(
-                            input[rowIndex][numberStartingIndex - 1]
-                        )
-                    if columnIndex + length != len(row):
-                        #getting the symbol to the right of the number
-                        borderingSymbols.append(
-                            input[rowIndex][numberStartingIndex + length]
-                        )
-                    if rowIndex != 0:
-                        #getting the symbol above the number
-                        borderingSymbols.append(
-                            input[rowIndex - 1][
-                                numberStartingIndex - 1 : numberStartingIndex
-                                + length
-                            ]
-                        )
-                    if rowIndex + 1 != len(input):
-                        #getting the symbol below the number
-                        borderingSymbols.append(
-                            input[rowIndex + 1][
-                                numberStartingIndex - 1 : numberStartingIndex
-                                + length
-                            ]
-                        )
-
-                    print("".join([i for i in borderingSymbols if i != "\n"]))
-                    
-                    if any(
-                        [
-                            symbol in borderingSymbols
-                            for symbol in ["*", "#", "$", "+", "&", "@"]
-                        ]
-                    ):
-                        substring = input[rowIndex][
-                            numberStartingIndex : numberStartingIndex + length
-                        ]
-                        try:
-                            total += int(substring)
-                        except ValueError:
-                            print(f"Cannot convert {substring} to an integer.")
-                    length = 0
-                    numberStartingIndex = None
-    return total
+def isSymbol(row: int, column: int) -> bool:
+    return (
+        row >= 0
+        and row < len(inputFile)
+        and column >= 0
+        and column < len(inputFile[row])
+        and inputFile[row][column] in ["*", "#", "$", "+", "&", "@"]
+    )
 
 
-print(Day3(input))
+def hasSymbolNeighbor(row: int, column: int) -> bool:
+    return (
+        isSymbol(row, column - 1)
+        or isSymbol(row, column + 1)
+        or isSymbol(row - 1, column - 1)
+        or isSymbol(row - 1, column)
+        or isSymbol(row - 1, column + 1)
+        or isSymbol(row + 1, column - 1)
+        or isSymbol(row + 1, column)
+        or isSymbol(row + 1, column + 1)
+        or isSymbol(row, column)
+    )
+
+result = [
+    [hasSymbolNeighbor(i, j) for j in range(len(inputFile[i]))]
+    for i in range(len(inputFile))
+]
+
+# def Day3(input: List[str]) -> int:
+#     total = 0
+#     for rowIndex, row in enumerate(input):
+#         for columnIndex, char in enumerate(row):
+
+#     return total
+
+
+# print(Day3(inputFile))
